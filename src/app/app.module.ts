00929a26a1504ef2d01337bc406dd9b6cc6a031c
Hüseyin ALTUNEL <65026269+husoaltunel@users.noreply.github.com>
2020-07-26 14:31:05 +0300

    Generating New Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MovieComponent } from './movie/movie.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule
